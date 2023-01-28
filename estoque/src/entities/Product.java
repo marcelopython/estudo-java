10aa8bed72376ce9622f7e4793e790caf11d4224
marcelopython <msr88957@gmail.com>
2023-01-27 23:36:29 -0300

    vetores

--- estoque/src/entities/Product.java
+++ estoque/src/entities/Product.java
@@ -2,11 +2,35 @@ package entities;
 
 public class Product {
 
-	public String name;
+	private String name;
 	
-	public double price;
+	private double price;
 	
-	public int quantity;
+	private int quantity;
+	
+	public Product() {
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
 	
 	public double totalValueInStock() {
 	
---
+++ referencia/src/referencia/Program.java
@@ -0,0 +1,13 @@
+package referencia;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int p;
+		p = 10;
+		System.out.println(p);
+
+	}
+
+}
