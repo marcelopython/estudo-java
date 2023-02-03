b464a6e56e95676ccc46c04df854556199481042
marcelopython <msr88957@gmail.com>
2023-02-03 13:27:00 -0300

    list filders

--- file/src/application/Program.java
+++ file/src/application/Program.java
@@ -15,13 +15,34 @@ public class Program {
 //		readFile();
 		
 //		Escrever aqruivo
-		fileWriter();
-
+//		fileWriter();
+		
+//		Pasta
+		folder();
 		
 		
 	}
 	
 	public static void folder() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		
+		System.out.println("FOLDERS");
+		
+		for (File folder : folders) {
+			
+			System.out.println(folder);
+			
+		}
+		
+		sc.close();
 		
 	}
 	
