64eeb9e20b0212c0361b170e54d789a1481b1faf
marcelopython <msr88957@gmail.com>
2023-02-07 22:59:00 -0300

    tipos curingas

---
+++ tipoCuringa/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+//		List<?> myObjs = new ArrayList<Object>();
+//		List<Integer> myNumbers = new ArrayList<Integer>();
+//		myObjs = myNumbers;
+		
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+
+		List<String> myStrs = Arrays.asList("Marcelo", "Silva", "Rodigues");
+		printList(myStrs);
+		
+	}
+	
+	
+	public static void printList(List<?> list) {
+		
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
+		
+	}
+	
+	
+}
