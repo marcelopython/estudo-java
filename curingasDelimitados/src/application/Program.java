60e1c198ddae325f27c97ae09553839f7467051f
marcelopython <msr88957@gmail.com>
2023-02-07 23:17:28 -0300

    coringa delimitados

---
+++ curingasDelimitados/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Shape> myShapes = new ArrayList<>();
+		myShapes.add(new Rectangle(3.0, 2.0));
+		myShapes.add(new Circle(2.0));
+		
+
+		System.out.println("Total area: "+ totalArea(myShapes));
+		
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(2.0));
+		myCircle.add(new Circle(3.0));
+
+		System.out.println("Total area: "+ totalArea(myCircle));
+		
+
+	}
+	
+//	Pode ser de Shape ou de qualquer tipo que seja subtipo de shape
+	public static double totalArea(List<? extends Shape> list) {
+		
+		double sum = 0.0;
+		for(Shape s : list) {
+			sum += s.area();
+		}
+		return sum;
+		
+	}
+
+}
