4da22e5882ee5baaaadc1b8be846fccf926198cb
marcelopython <msr88957@gmail.com>
2023-02-12 21:07:05 -0300

    Comparable

--- set/src/application/Program.java
+++ set/src/application/Program.java
@@ -1,9 +1,9 @@
 package application;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
+
+import entities.Product;
 
 public class Program {
 
@@ -34,24 +34,47 @@ public class Program {
 //			
 //		}
 		//COnjuntos
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+//		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+//		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+//		
+//		
+////		Union
+//		Set<Integer> c = new TreeSet<>(a);
+//		c.addAll(b);
+//		System.out.println(c);
+//		
+////		intersection (são somente os elementos que tem em comun no elemento)
+//		Set<Integer> d = new TreeSet<>(a);
+//		d.retainAll(b);
+//		System.out.println(d);
+//		
+////		Difference
+//		Set<Integer> e = new TreeSet<>(a);
+//		e.removeAll(b);
+//		System.out.println(e);
 		
 		
-//		Union
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
+//		Comparar objetos 
+//		Set<Product> set = new HashSet<>();
+//		
+//		set.add(new Product("TV", 900.0));
+//		set.add(new Product("Notebook", 1200.0));
+//		set.add(new Product("Tablet", 400.0));
+//		
+//		Product prod = new Product("Notebook", 1200.0);
+////		checagem set tem o objeto producto 
+//		System.out.println(set.contains(prod));
 		
-//		intersection (são somente os elementos que tem em comun no elemento)
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println(d);
+		Set<Product> set = new TreeSet<>();
 		
-//		Difference
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println(e);
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));		
+		
+		for(Product p : set ) {
+			System.out.println(p);
+		}
+		
 		
 	}
 
